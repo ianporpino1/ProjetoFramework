0f69eaa7d73de3a50506e314d2cb192eac2eb15f
ianporpino1 <ianporpino127@gmail.com>
2023-12-12 16:10:49 -0300

    ajustes

--- src/main/java/com/corretora/dao/PosicaoRepository.java
+++ src/main/java/com/corretora/dao/PosicaoRepository.java
@@ -21,7 +21,7 @@ public interface PosicaoRepository extends JpaRepository<Posicao, Long> {
     Posicao findPosicaoByIdAtivo(@Param("idAtivo") Long idAtivo,@Param("idUsuario")Long idUsuario);
 
     @Query(value = "SELECT a.identificador, TRUNCATE(p.preco_medio, 2) AS preco_medio, p.quantidade_total, " +
-                    "TRUNCATE(p.valor_total, 2) AS valor_total FROM posicao p JOIN ativo a ON p.id_ativo = a.id WHERE p.id_usuario = :idUsuario AND p.status_posicao=1", nativeQuery = true)
+                    "TRUNCATE(p.valor_total, 2) AS valor_total FROM posicao p JOIN ativo a ON p.id_ativo = a.id WHERE p.id_usuario = :idUsuario AND p.status_posicao=0", nativeQuery = true)
     List<Object[]> findFormattedPosicoes(@Param("idUsuario")Long idUsuario);
     @Modifying
     @Query(value = "delete FROM ativo a WHERE a.identificador = :identificador", nativeQuery = true)
