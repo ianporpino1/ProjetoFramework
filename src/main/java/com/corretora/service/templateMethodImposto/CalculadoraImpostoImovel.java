f91dcc6a5d403cb42b628eaa69f2297e73e4c231
ianporpino1 <ianporpino127@gmail.com>
2023-12-12 13:01:48 -0300

    ajustes finais

--- src/main/java/com/corretora/service/templateMethodImposto/CalculadoraImpostoImovel.java
+++ src/main/java/com/corretora/service/templateMethodImposto/CalculadoraImpostoImovel.java
@@ -5,18 +5,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class CalculadoraImpostoImovel extends CalculadoraImposto {
 
-    private final double ALIQUOTA_IMPOSTO_ACOES_BR_1 = 0.15;
-    private final double ALIQUOTA_IMPOSTO_ACOES_BR_2 = 0.175;
-    private final double ALIQUOTA_IMPOSTO_ACOES_BR_3 = 0.2;
-    private final double ALIQUOTA_IMPOSTO_ACOES_BR_4 = 0.225;
+    private final double ALIQUOTA_IMPOSTO_IMOVEIS_BR_1 = 0.15;
+    private final double ALIQUOTA_IMPOSTO_IMOVEIS_BR_2 = 0.175;
+    private final double ALIQUOTA_IMPOSTO_IMOVEIS_BR_3 = 0.2;
+    private final double ALIQUOTA_IMPOSTO_IMOVEIS_BR_4 = 0.225;
 
-    private final double VALOR_ISENCAO_ACOES_BRASIL = 35000;
+    private final double VALOR_ISENCAO_IMOVEIS_BRASIL = 35000;
 
     @Override
     public boolean calcularIsencao(double volume) {
-        if(volume <= VALOR_ISENCAO_ACOES_BRASIL)
+        if(volume <= VALOR_ISENCAO_IMOVEIS_BRASIL)
             return true;
-        else if(volume > VALOR_ISENCAO_ACOES_BRASIL){
+        else if(volume > VALOR_ISENCAO_IMOVEIS_BRASIL){
             return false;
         }
         return false;
@@ -27,16 +27,16 @@ public class CalculadoraImpostoImovel extends CalculadoraImposto {
         if(total > 0){
             double novoImposto = 0;
             if(total <= 5000000){
-                novoImposto = total * ALIQUOTA_IMPOSTO_ACOES_BR_1;
+                novoImposto = total * ALIQUOTA_IMPOSTO_IMOVEIS_BR_1;
             }
             else if(total > 50000000 && total <= 100000000){
-                novoImposto = total * ALIQUOTA_IMPOSTO_ACOES_BR_2;
+                novoImposto = total * ALIQUOTA_IMPOSTO_IMOVEIS_BR_2;
             }
             else if(total > 100000000 && total <= 300000000){
-                novoImposto = total * ALIQUOTA_IMPOSTO_ACOES_BR_3;
+                novoImposto = total * ALIQUOTA_IMPOSTO_IMOVEIS_BR_3;
             }
             else if(total >300000000){
-                novoImposto = total * ALIQUOTA_IMPOSTO_ACOES_BR_4;
+                novoImposto = total * ALIQUOTA_IMPOSTO_IMOVEIS_BR_4;
             }
 
             novoImposto = novoImposto*100;
